4da5bc6522a6b901d8e0896b3770e98c871ae293
Virtuso1225 <dominico1225@gmail.com>
2024-02-29 18:57:59 +0900

    mod: modify server status code

--- DevKor_BE_Basic/server.js
+++ DevKor_BE_Basic/server.js
@@ -47,7 +47,8 @@ server.patch('/todo/update/:id', async (req, res) => {
         const sqlQuery = 'UPDATE todoList SET isChecked = ? WHERE id = ?';
         const data = [isChecked, id];
         const [rows] = await pool.query(sqlQuery, data);
-        res.status(201).json(isChecked)
+        const message = isChecked ? 'TODO가 완료되었습니다!' : 'TODO가 최소되었습니다!';
+        res.status(201).json({code: 201, message, data: null, statusCode: 201})
         connection.release();
     } catch(error){
         res.sendStatus(500);
@@ -71,13 +72,13 @@ server.delete('/todo/delete/:id', async (req, res) => {
 server.get('/todo/secure/lists', auth, async(_, res)=>{
     try{
         const connection = await pool.getConnection(async conn => conn);
-        const sqlQuery = 'SELECT * FROM todoList';
+        const sqlQuery = `SELECT id, content, IF(isChecked, 'true', 'false') as isChecked FROM todoList`;
         const [rows] = await pool.query(sqlQuery);
-        res.status(200).json(rows)
+        res.status(200).json({code: 200, message: 'TODO가 성공적으로 조회되었습니다!', data: rows, statusCode: 200})
         connection.release();
     }
     catch(error){
-        res.sendStatus(500);
+        res.status(500).json({code: 'ERROR', message: '서버 에러가 발생했습니다. 다시 시도해주세요.', data: null, statusCode: 500});
     }
 })
 
@@ -88,10 +89,10 @@ server.post('/todo/secure/create', auth, async (req, res) => {
         const sqlQuery = 'INSERT INTO todoList (content) VALUES (?)';
         const data = [content];
         const [rows] = await pool.query(sqlQuery, data);
-        res.status(201).json({result: rows})
+        res.status(201).json({code: 201, message: 'TODO가 생성되었습니다!', data: rows, statusCode: 201})
         connection.release();
     } catch(error){
-        res.sendStatus(500);
+        res.status(500).json({code: 'ERROR', message: '서버 에러가 발생했습니다. 다시 시도해주세요.', data: null, statusCode: 500});
     }
 })
 
@@ -103,10 +104,11 @@ server.patch('/todo/secure/update/:id', auth, async (req, res) => {
         const sqlQuery = 'UPDATE todoList SET isChecked = ? WHERE id = ?';
         const data = [isChecked, id];
         const [rows] = await pool.query(sqlQuery, data);
-        res.status(201).json(isChecked)
+        const message = isChecked ? 'TODO가 완료되었습니다!' : 'TODO가 최소되었습니다!';
+        res.status(201).json({code: 201, message, data: null, statusCode: 201});        
         connection.release();
     } catch(error){
-        res.sendStatus(500);
+        res.status(500).json({code: 'ERROR', message: '서버 에러가 발생했습니다. 다시 시도해주세요.', data: null, statusCode: 500});
     }
 })
 
@@ -117,10 +119,10 @@ server.delete('/todo/secure/delete/:id', auth, async (req, res) => {
         const sqlQuery = 'DELETE FROM todoList WHERE id = ?';
         const data = [id];
         const [rows] = await pool.query(sqlQuery, data);
-        res.status(200).json({result: rows})
+        res.status(200).json({code: 200, message: 'TODO가 성공적으로 삭제되었습니다!', data: rows, statusCode: 200})
         connection.release();
     } catch(e){
-        res.sendStatus(500);
+        res.status(500).json({code: 'ERROR', message: '서버 에러가 발생했습니다. 다시 시도해주세요.', data: null, statusCode: 500});
     }
 })
 
@@ -143,7 +145,7 @@ server.post('/create/user', async(req, res)=> {
 server.post('/login', async(req, res)=>{
     const {username, password} = req.body;
     if(!username || !password){
-        return res.sendStatus(400);
+        return res.status(400).json({code:400, message: '아이디와 비밀번호를 입력해주세요.', data: null, statusCode: 400});
     }
     try{
         const connection = await pool.getConnection(async conn => conn);
@@ -151,7 +153,7 @@ server.post('/login', async(req, res)=>{
         const data = [username];
         const [rows] = await pool.query(sqlQuery, data);
         if(rows.length === 0){
-            res.status(400).json({message: '존재하지 않는 유저입니다.'});
+            res.status(400).json({code:400, message: '존재하지 않는 유저입니다.', data: null, statusCode: 400});
             connection.release();
             return;
         }
@@ -166,22 +168,22 @@ server.post('/login', async(req, res)=>{
             if(isExist.length !== 0){
                 const deleteQuery = 'UPDATE tokens SET refresh = ? WHERE username = ?';
                 await pool.query(deleteQuery, [refreshToken, username]);
-                res.status(200).json({accessToken, refreshToken, expiresAt});
+                res.status(200).json({code: 'SUCCESS', message: '로그인에 성공하였습니다!',data: {accessToken, refreshToken, expiresAt}, statusCode: 200});
                 connection.release();
                 return;
             }
             const sqlQuery = 'INSERT INTO tokens (username, refresh) VALUES (?,?)';
             const data = [username, refreshToken];        
             await pool.query(sqlQuery, data);
-            res.status(200).json({accessToken, refreshToken, expiresAt});
+            res.status(200).json({code: 'SUCCESS', message: '로그인에 성공하였습니다!',data: {accessToken, refreshToken, expiresAt}, statusCode: 200});
             connection.release();
         }else{
-            res.status(400).json({message: '비밀번호가 일치하지 않습니다.'});
+            res.status(400).json({code: 'FAIL',message: '비밀번호가 일치하지 않습니다!!', data: null, statusCode: 400});
             connection.release();
         }
     }catch(error){
         console.log(error)
-        res.sendStatus(500);
+        res.status(500).json({code: 'ERROR', message: '서버 에러가 발생했습니다. 다시 시도해주세요.', data: null, statusCode: 500});
     }
 })
 
@@ -194,10 +196,10 @@ server.post('/logout', auth, async(req, res)=>{
         const sqlQuery = 'DELETE FROM tokens WHERE username = ?';
         const data = [user];
         const [rows] = await pool.query(sqlQuery, data);
-        res.sendStatus(200);
+        res.status(200).json({code: 'SUCCESS', message: '로그아웃에 성공하였습니다!', data: null, statusCode: 200});
         connection.release();
     }catch(error){
-        res.sendStatus(500);
+        res.status(500).json({code: 'ERROR', message: '서버 에러가 발생했습니다. 다시 시도해주세요.', data: null, statusCode: 500});
     }
 })
 
